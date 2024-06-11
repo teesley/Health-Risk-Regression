5047452af6a254c98f49653bf9e50a8541c03058
Trevor Beesley <trevor.beesley@iclud.com>
2024-06-10 20:21:22 -0600

    Added the .py file for the regression model

---
+++ Regression.py
@@ -0,0 +1,46 @@
+# %%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# %%
+df = pd.read_csv('./data/Maternal_Health_Risk_Data_Set.csv')
+
+# %%
+X_features = ['Age','SystolicBP','DiastolicBP','BS', 'BodyTemp', 'HeartRate']
+df['RiskLevel'].value_counts()
+df.drop(df[df['HeartRate'] < 50].index)
+RiskLabels = ['low risk', 'mid risk', 'high risk']
+df
+
+# %%
+fig,axes=plt.subplots(1, 6, figsize=(12, 3), sharey=True)
+for i in range(len(axes)):
+    sns.stripplot(data= df, x=X_features[i], y=df['RiskLevel'], ax=axes[i])
+
+# %%
+from sklearn import model_selection
+from sklearn import linear_model
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+# %%
+X = df.loc[: , df.columns!='RiskLevel']
+y = df['RiskLevel']
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size= 0.2, random_state = 1)
+
+print('X_train dimension= ', X_train.shape)
+print('X_test dimension= ', X_test.shape)
+print('y_train dimension= ', y_train.shape)
+print('y_train dimension= ', y_test.shape)
+
+# %%
+rfc = RandomForestClassifier()
+rfc.fit(X_train, y_train)
+print(rfc.score(X_test, y_test))
+
+# %%
+
+
+
