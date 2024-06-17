2c044024302d37b535d4ec2f3c0f076411ae00c8
Trevor Beesley <trevor@chaosplan.com>
2024-06-17 16:48:04 -0600

    Added navbar and future pages, added more styling and fixed the text that was displayed when a prediction was made

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+#from flask_nav import Nav
 import pickle
+import re
 
 app = Flask(__name__)
 
@@ -8,14 +10,24 @@ model = pickle.load(open("model.pkl", "rb"))
 
 @app.route("/") 
 def Home():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/data")
+def dataDet():
+    return render_template("data_details.html")
+
+@app.route("/model")
+def modelDet():
+    return render_template("model_details.html")
 
 @app.route("/predict", methods = ["POST"])
 def predict():
     x_features = [float(x) for x in request.form.values()]
     features = [np.array(x_features)]
     prediction = model.predict(features)
-    return render_template("index.html", prediction_text = "Maternal Health risk is {}".format(prediction))
+    text = "Maternal Health risk is {}".format(prediction)
+    text = re.sub(r'[\[\]\']', '', text)
+    return render_template("home.html", prediction_text = text)
 
 if __name__ == '__main__':
-    app.run(debug=True)      
+    app.run(debug=True, port=5000)      
